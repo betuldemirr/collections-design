b207f6b18522b7b2d0cb83b6f4d246a93697bd46
Betül Demir <betuldemir.007@gmail.com>
2022-08-04 11:58:47 +0300

    commit

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 
 function display(event, tabName) {
     var tabContent = document.getElementsByClassName("container--menu-content")
-    var tabLinks = document.getElementsByClassName("container--menu-list")
+    //var tabLinks = document.getElementsByClassName("container--menu-list")
 
     for (var i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
-      }
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace("active", "");
     }
-
     document.getElementById(tabName).style.display = "block";
-    event.style.backgroundColor = "red"
 }
